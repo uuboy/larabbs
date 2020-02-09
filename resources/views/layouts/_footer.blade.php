6d38cabf6c8bbe3c8c62126fd39f3173facec61d
uuboy <uu_boy@foxmail.com>
2020-02-09 08:09:19 +0000

    20200209

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,7 +3,7 @@
         <p class="pull-left">
              <a href="https://hagy@naiveuu.com" target="_blank">宁波市辉昂管业有限公司</a> 版权所有
         </p>
-        <p class="pull-left" style="margin-left: 20px;">
+        <p class="pull-left" style="padding-left: 20px;">
             <a href="http://www.beian.miit.gov.cn">浙ICP备19040425号-1</a>
         </p>
 
